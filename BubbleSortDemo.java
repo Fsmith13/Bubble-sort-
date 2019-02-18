e748208c00e0331980769f6a4848524865b7a87a
Fsmith13 <frankieasmith@gmail.com>
2019-02-18 15:20:00 -0500

    Add files via upload

---
+++ BubbleSortDemo.java
@@ -0,0 +1,23 @@
+// Filename: BubbleSortdemo.java
+// Author: Frank
+// Date: 2/12/19
+
+import java.util.*;
+
+public class BubbleSortdemo
+{
+	public static void main(String[] args) 
+	{
+		// variables and constants
+		int[] someNums = new int[5];
+		int comparisonsToMake = someNums.length - 1;
+		Scanner keyboard = new Scanner(System.in);
+		int a, b, temp;
+
+		// for loop that will ask the user to enter in up to five numbers we will store those numbers in the someNums array
+		for(a = 0; a < someNums.length; a++)
+		{
+			
+		}
+	}
+}
